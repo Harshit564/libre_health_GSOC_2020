49fed8add3c3289f71e08539b00223ea2642271f
Harshit Singh <47476857+Harshit564@users.noreply.github.com>
2020-03-27 09:38:19 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:librehealth/Constant/constant.dart';
+import 'package:librehealth/Pages/IntroPage.dart';
 import 'package:librehealth/Pages/LoginPage.dart';
 import 'package:librehealth/Pages/SplashScreen.dart';
 import 'package:librehealth/Screens/BirthList.dart';
@@ -14,13 +15,13 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'LibreHealth',
       debugShowCheckedModeBanner: false,
-      theme: new ThemeData.dark(),
+      theme: new ThemeData.light(),
       home: AnimatedSplashScreen(),
       routes: <String, WidgetBuilder>{
         SPLASH_SCREEN: (BuildContext context) => AnimatedSplashScreen(),
         HOME_SCREEN: (BuildContext context) => NoteList(),
         LOGIN_SCREEN: (BuildContext context) => LoginPage(),
-        
+        INTRO_SCREEN:(BuildContext context) => IntroPage(),
       },
     );
   }

56d40cba7b57da4a65090b6728a92bf59efb265e
Harshit Singh <47476857+Harshit564@users.noreply.github.com>
2020-03-27 09:42:05 +0530

    Add files via upload

---
+++ lib/Pages/IntroPage.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:librehealth/Pages/LoginPage.dart';
+
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  SwiperController _controller = SwiperController();
+  int _currentIndex = 0;
+  final List<String> titles = [
+    "Welcome",
+    "Secure Registration",
+    "Track Vaccinations",
+  ];
+  final List<String> subtitles = [
+    "Birth registration app for Kenya residents",
+    "We provide easy and secure way for birth registration",
+    "We will help in tracking essential vaccinations for newborns"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Swiper(
+            loop: false,
+            index: _currentIndex,
+            onIndexChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            controller: _controller,
+            pagination: SwiperPagination(
+              builder: DotSwiperPaginationBuilder(
+                activeColor: Colors.black,
+                activeSize: 20.0,
+              ),
+            ),
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return IntroItem(
+                title: titles[index],
+                subtitle: subtitles[index],
+                bg: Colors.orange,
+                imageUrl: "images/${index + 1}.png",
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FlatButton(
+              child: Text("Skip"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: IconButton(
+              icon:
+                  Icon(_currentIndex == 2 ? Icons.check : Icons.arrow_forward),
+              onPressed: () {
+                if (_currentIndex != 2)
+                  _controller.next();
+                else
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class IntroItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Color bg;
+  final String imageUrl;
+
+  const IntroItem(
+      {Key key, @required this.title, this.subtitle, this.bg, this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: bg ?? Theme.of(context).primaryColor,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 40),
+              Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.white),
+              ),
+              if (subtitle != null) ...[
+                const SizedBox(height: 20.0),
+                Text(
+                  subtitle,
+                  style: TextStyle(color: Colors.white, fontSize: 24.0),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+              const SizedBox(height: 40.0),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 70),
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Material(
+                      elevation: 4.0,
+                      child: Image.asset(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
